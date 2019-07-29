3382f81c1ead267a56481ea657140bdb6303a4cf
Pavel-Sidorovich <sidorovichpavelalex@gmail.com>
2019-07-29 17:55:39 +0300

    Completed hometask_2

--- app/src/main/java/com/pavesid/devintensive/ui/activities/MainActivity.kt
+++ app/src/main/java/com/pavesid/devintensive/ui/activities/MainActivity.kt
@@ -1,54 +1,18 @@
 package com.pavesid.devintensive.ui.activities
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import com.pavesid.devintensive.R
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(){
 
-    companion object {
-        const val COLOR_MODE_KEY = "COLOR_MODE_KEY"
-    }
-
-    private var editText : EditText? = null
-    private var redButton : Button? = null
-    private var greenButton : Button? = null
-    private var colorMode : Int = 0
-
-//    init {
-//        redButton = findViewById(R.id.red_btn)
-//        greenButton = findViewById(R.id.green_btn)
-//        editText = findViewById(R.id.textView)
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("M_MainActivity", "onCreate")
 
-        redButton = findViewById(R.id.red_btn)
-        greenButton = findViewById(R.id.green_btn)
-        editText = findViewById(R.id.textView)
-
-        redButton?.setOnClickListener(this)
-        greenButton?.setOnClickListener(this)
-
-        if(savedInstanceState == null){
-            //Активити впервые запускается
-        } else {
-            colorMode = savedInstanceState.getInt(COLOR_MODE_KEY)
-            Log.d("M_MainActivity", "$colorMode")
-            when(colorMode){
-                Color.RED -> editText?.setBackgroundColor(Color.RED)
-                Color.GREEN -> editText?.setBackgroundColor(Color.GREEN)
-            }
-        }
     }
 
     override fun onStart() {
@@ -76,23 +40,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Log.d("M_MainActivity", "onDestroy")
     }
 
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.green_btn -> {
-                editText?.setBackgroundColor(Color.GREEN)
-                colorMode = Color.GREEN
-            }
-            R.id.red_btn -> {
-                editText?.setBackgroundColor(Color.RED)
-                colorMode = Color.RED
-            }
-        }
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
         Log.d("M_MainActivity", "onSaveInstanceState")
-        outState.putInt(COLOR_MODE_KEY, colorMode)
     }
 }
